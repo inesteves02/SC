67f38391ec3d4cdae45ac899f64483fdaa49a124
Gacos11 <vascotbarros1@gmail.com>
2023-03-12 03:14:27 +0000

    Classe melhorada e corrigida

--- domain/User.java
+++ domain/User.java
@@ -8,9 +8,41 @@ public class User {
     private double balance;
     private HashMap<String, Wine> wines;
 
+    // Construtor para criar um novo utilizador
     public User(String name) {
         this.name = name;
         this.balance = 200;
-        this.wines = new HashMap<String, Wine>();
+        this.wines = new HashMap<>();
+    }
+
+    // Construtor para criar um utilizador a partir de um ficheiro
+    public User(String name, double balance, HashMap<String, Wine> wines) {
+        this.name = name;
+        this.balance = balance;
+        this.wines = wines;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public HashMap<String, Wine> getWines() {
+        return wines;
+    }
+
+    public boolean haveWine(String name) {
+        return wines.containsKey(name);
+    }
+
+    public void addWine(Wine wine) {
+        wines.put(wine.getName(), wine);
+    }
+
+    public void setWines(HashMap<String, Wine> wines) {
+        this.wines = wines;
     }
 }
