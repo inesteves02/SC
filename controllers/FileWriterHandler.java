6a5ae98dacfecfe141ba0ea42f8574c2f9f5026d
Gacos11 <vascotbarros1@gmail.com>
2023-03-12 03:13:33 +0000

    Metodos adicionados

--- controllers/FileWriterHandler.java
+++ controllers/FileWriterHandler.java
@@ -1,16 +1,47 @@
 package controllers;
 
+import domain.User;
+import domain.Wine;
 import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 
 public class FileWriterHandler {
 
-    private File file;
     private final String LOGIN_FILE = "login.txt";
+    private final String USER_DATA_FOLDER = "user_data";
+    private final String USER_FILE = "User.txt";
+    private final String WINE_FILE = "Wine.txt";
+    private final String MESSAGE_FILE = "Message.txt";
     
     public FileWriterHandler () {
         create_verify_logintxt();
+        create_user_data();
+    }
+
+    private void create_user_data() {
+        File file = new File(USER_DATA_FOLDER);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+    }
+
+    public void addWineToUser(User user, Wine wine) {
+        // cria um objeto File com o caminho da pasta do usuário
+        File userDirectory = new File(USER_DATA_FOLDER, user.getName());
+
+        // cria um objeto File com o caminho do arquivo do vinho
+        File wineFile = new File(userDirectory, "Wine.txt");
+
+        // escreve as informações do vinho no arquivo
+        try {
+            FileWriter writer = new FileWriter(wineFile);
+            writer.write(wine.getName() + ":" + wine.getImage() + ":" + wine.getPrice() + ":" + wine.getQuantity() + ":" + wine.getRating() + ":" + wine.isForSale() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /*
@@ -18,7 +49,7 @@ public class FileWriterHandler {
      * If an IOException is thrown, an error message is printed.
      */
     private void create_verify_logintxt () {
-        File file = new File("login.txt");
+        File file = new File(LOGIN_FILE);
         try {
             if (file.createNewFile()) {
                 System.out.println("The file login.txt was created.");
@@ -30,6 +61,8 @@ public class FileWriterHandler {
         }
     }
 
+
+
     /*
      * This code adds a user to the LOGIN_FILE file. It takes two parameters: a username (String user) and a password (String pass). 
      * It uses a FileWriter to write the username and password to the file, separated by a colon (":"). If an IOException is thrown, an error message is printed.
@@ -39,6 +72,40 @@ public class FileWriterHandler {
             writer.write(user + ":" + pass + "\n");
         } catch (IOException e) {
             System.err.println("Error writing to login.txt: " + e.getMessage());
+        }
+    }
+
+    public void createUserFolderAndFiles(User user) {
+        File file = new File(USER_DATA_FOLDER, user.getName());
+        if (!file.exists()) {
+            file.mkdir();
+
+            String path = USER_DATA_FOLDER + "/" + user + "/";
+
+            File userFile = new File(path, USER_FILE);
+            File wineFile = new File(path, WINE_FILE);
+            File messageFile = new File(path, MESSAGE_FILE);
+
+            try {
+                userFile.createNewFile();
+                wineFile.createNewFile();
+                messageFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // escreve as informações do usuário no arquivo
+            try {
+                FileWriter fw = new FileWriter(path + USER_FILE);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                bw.write("Balance: " + user.getBalance());
+                bw.newLine();
+                bw.close();
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
