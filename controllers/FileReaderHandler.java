ee8fc188dabbd7181e147e243823f8e74367c6af
Gacos11 <vascotbarros1@gmail.com>
2023-03-12 03:13:18 +0000

    Criado função para ler ficheiros ao iniciar - NAO FUNCIONA AINDA

--- controllers/FileReaderHandler.java
+++ controllers/FileReaderHandler.java
@@ -4,17 +4,47 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+
+import domain.User;
+import domain.Wine;
 
 public class FileReaderHandler {
 
     private final String LOGIN_FILE = "login.txt";
+    private final String USER_DATA_FOLDER = "user_data";
+    private final String USER_FILE = "User.txt";
+    private final String WINE_FILE = "Wine.txt";
+    private final String MESSAGE_FILE = "Message.txt";
 
+    //Ler todos os usuarios e os seus respetivos vinhos e criar um hashmap com os mesmos
+    public HashMap<String, User> readUsers() {
+
+        HashMap<String, User> users = new HashMap<>();
+
+        File folder = new File(USER_DATA_FOLDER);
+
+        if (folder.isDirectory()) { // Verifica a existência da pasta user_data
+            File[] folders = folder.listFiles(); // Lista todos as pastas da pasta user_data
+            for (File f : folders) {
+                if (f.isDirectory()) {
+                    File[] files2 = f.listFiles(); // Lista todos os arquivos da pasta do usuário
+
+                    //Busca o valor do arquivo User.txt
+                    
+                    // Cria um objeto User com o nome do usuário
+                    User user = new User(f.getName(), );
+                }
+            }
+        }
+
+    }
     /*
      * This code reads the LOGIN_FILE file and checks if the username and password match. 
      * It takes two parameters: a username (String user) and a password (String pass).
      */
     public int clientLogin(String user, String pass) {
-        File file = new File("login.txt");
+        File file = new File(LOGIN_FILE);
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
